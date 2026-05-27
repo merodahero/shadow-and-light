4e54e92baa6f9abb599a23a0ead426802f9fe405
merodahero <merodahero@gmail.com>
2026-05-28 00:13:52 +0300

    fix diagnostic comments in cache.lua for consistency

--- ElvUI_SLE/core/cache.lua
+++ ElvUI_SLE/core/cache.lua
@@ -1,6 +1,4 @@
-﻿-- @diagnostic disable:not-local-init
--- @diagnostic disable=undefined-global
-local SLE, T, E, L, V, P, G = unpack(ElvUI_SLE)
+﻿local SLE, T, E, L, V, P, G = unpack(ElvUI_SLE)
 local _G = _G
 
 T.MapInfoTable = {
